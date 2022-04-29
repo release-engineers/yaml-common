be58ed1405c3267bdf26fa9745cc724e993e0913
Philipp Gayret <philipp@hileco.com>
2022-04-29 23:51:02 +0200

    Added common yaml and empty yaml

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('yo')
+#!/usr/bin/env python
+
+import sys
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+
+data_by_filename = {}
+
+for filename in sys.argv[1:]:
+    with open(filename, 'r') as f:
+        data = yaml.load(f)
+        data_by_filename[filename] = data
+
+for filename, data in data_by_filename.items():
+    print('---')
+    print('# {}'.format(filename))
+    print('')
+    yaml.dump(data, sys.stdout)
